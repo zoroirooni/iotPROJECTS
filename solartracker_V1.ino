0ba72bc305c176e315f0ef5f409acb3ada85dd67
Reza Hajebi <reza@litbit.com>
2016-03-16 14:01:19 -0700

    Update solartracker_V1.ino

--- solartracker_V1.ino
+++ solartracker_V1.ino
@@ -2,28 +2,11 @@
   Mobile Sun-Tracking Solar Power Plant
   Arduino Microcontroller
   
-  This program is free software: you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation, either version 3 of the License, or
-  any later version.
-
-  This program is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
-
-  See <http://www.gnu.org/licenses/>.
-
+  
   ****************************
 
-  Created Sept 19, 2013
-  Revision 1.2
-  Kurt Schulz
   
-  Update History:
-
-  Rev 1.2  09/19/2013
-  : Initial version
+  Rev 1.2 
 
 *****************************/
 #include <Servo.h>
